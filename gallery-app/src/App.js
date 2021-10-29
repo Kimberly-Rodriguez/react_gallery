366d80a1f8e29aa95c02e34d406ef90aa985f998
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-10-28 17:19:56 -0700

    confirming path from index.js to App.js and Gallery Components

---
+++ gallery-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Gallery from './components/Gallery'; 
+
+function App() {
+  return <Gallery/>;
+}
+
+
+export default App;
---
+++ gallery-app/src/components/Gallery.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Gallery() {
+  const message= 'Hello world! I am going to render a picture soon';
+ 
+ 
+  return (
+    <div className="container">
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
