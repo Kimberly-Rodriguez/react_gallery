aa314cff37ab3e6f36b22dd2f040077c0de9b50e
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-11-08 12:44:12 -0800

    mapping through group array + key for dinamic component rendering

--- gallery-app/src/components/Greeting.js
+++ gallery-app/src/components/Greeting.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 export default function Greeting() {
 const [greeting, setGreeting] = useState('Kimberly Rodriguez')
+const [group, setGroup] = useState(["John", "Grace", "Jared"]);
 
 return (
   <div className="card text-center">
@@ -12,6 +13,14 @@ return (
       <p className="card-text" style={{fontSize: '50px'}}>
         {greeting}
       </p>
+      <ul>
+        {group.map((group) => (
+         <li key={group.id}>{group}</li>
+        ))}
+        {/* <li>{group[1]}</li>
+        <li>{group[2]}</li> */}
+       
+      </ul>
     </div>
   </div>
 );
