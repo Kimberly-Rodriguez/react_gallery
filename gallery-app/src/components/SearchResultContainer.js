2e3376e2825543eb69de7d37e2f2ca5a4c8c8603
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-11-09 14:47:49 -0800

    composed the SearchResultContainer variable to use the giphy API and to be shared with the ResultList Component via the jax

--- gallery-app/src/components/SearchResultContainer.js
+++ gallery-app/src/components/SearchResultContainer.js
@@ -3,3 +3,24 @@ import ResultList from './ResultList';
 
 import search from '../utils/API';
 
+const SearchResultContainer = () => {
+  const [result, setResult] = useState([]);
+
+  const searchGiphy = async (query) => {
+    const response = await search(query);
+    setResult(response.data.data); 
+  };
+  
+  useEffect(() => {
+    searchGiphy('kittens'); 
+  }, []);
+
+  return (
+    <div>
+      <ResultList result={result} />
+    </div>
+  );
+};
+
+export default SearchResultContainer; 
+
