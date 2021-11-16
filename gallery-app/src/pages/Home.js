198d9f12c5814eadbd784ae65a95deb1325baf0c
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-11-16 11:27:54 -0800

    updated login and orderfor upcoming connection

--- gallery-app/src/pages/Home.js
+++ gallery-app/src/pages/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
               <div class="container px-5">
                   <h1 class="masthead-heading mb-0">Gallery</h1>
                   <h5 class="masthead-subheading mb-0">highlight key moments </h5>
-                  <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+                  <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Upload Image</a>
               </div>
           </div>
           <div class="bg-circle-1 bg-circle"></div>
